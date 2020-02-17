f03074c0383155a125ffa7efa4852df1e4212c79
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-02-17 22:07:41 +0900

    Don't use assert_output provided from CoreAssertions

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -2,3 +2,23 @@ require "test/unit"
 require_relative "core_assertions"
 
 Test::Unit::TestCase.include Test::Unit::CoreAssertions
+
+module Test
+  module Unit
+    class TestCase
+      def assert_output stdout = nil, stderr = nil
+        out, err = capture_output do
+          yield
+        end
+
+        err_msg = Regexp === stderr ? :assert_match : :assert_equal if stderr
+        out_msg = Regexp === stdout ? :assert_match : :assert_equal if stdout
+
+        y = send err_msg, stderr, err, "In stderr" if err_msg
+        x = send out_msg, stdout, out, "In stdout" if out_msg
+
+        (!stdout || x) && (!stderr || y)
+      end
+    end
+  end
+end
